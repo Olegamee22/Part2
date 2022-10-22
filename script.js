a9e305ef508c5f0305f03d7445d7fb99408e9438
Bonker <olegamee22@gmail.com>
2022-10-22 12:29:16 +0300

    29 lesson commit

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-"use sctrict";
+'use strict';
 
-alert("Hello");
+function calculateVolumeAndArea(a) {
+    
+    if (typeof (a) !== 'number' || a <= 0 || !Number.isInteger(a)) {
+        return console.log("При вычислении произошла ошибка");
+    }
+    const S = 6 * (a * a); 
+    const V = a * a * a;
+
+    return console.log(`Объем куба: ${V}, площадь всей поверхности: ${S}`);
+}
+
+calculateVolumeAndArea(1.2);
+
